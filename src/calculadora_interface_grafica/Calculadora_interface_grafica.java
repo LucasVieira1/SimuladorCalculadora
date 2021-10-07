c62e7c4175160637dca732680e6cc2adc7f9cde9
LucasVieira1 <vieiraalucas1@gmail.com>
2021-10-06 22:16:54 -0300

    Adicionando os arquivos iniciais do projeto

---
+++ src/calculadora_interface_grafica/Calculadora_interface_grafica.java
@@ -0,0 +1,9 @@
+package calculadora_interface_grafica;
+
+    import java.util.Scanner;
+    public class Calculadora_interface_grafica {
+    public static void main(String[] args) {
+        UI_Calculadora calculadora = new UI_Calculadora();
+        calculadora.setVisible(true);
+    } 
+}
